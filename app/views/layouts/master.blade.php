ebc2bdd082c1069f078542d4efa49818655c9515
travtex <tflatt74@gmail.com>
2014-03-24 14:55:39 -0500

    switching to blade views

---
+++ app/views/layouts/master.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Trav Flatt</title>
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+    @yield('topscript')
+</head>
+<body>
+    @yield('content')
+
+    @yield('bottomscript')
+</body>
+</html>
