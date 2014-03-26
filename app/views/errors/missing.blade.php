6a055dc88ce4841c1ffe096a556aeffe48c07013
Travis Flatt <tflatt74@gmail.com>
2014-03-25 22:19:12 -0500

    updated 404 page

--- app/views/errors/missing.blade.php
+++ app/views/errors/missing.blade.php
@@ -2,6 +2,15 @@
 
 @section('content')
 
-<h1>Oops, Page not found</h1>
+<div class="error_page">
+				<div class="row clearfix">
+					<div class="CLE">
+						<i class="icon_dislike errori"></i>
+						<h2 class="tac mtt"> PAGE NOT FOUND <small> The page you are looking for might have been removed, or 
+							had its name changed. </small></h2>
+						<a href="/" class="tbutton medium"><span><i class="icons-arrow-left mi"></i> Back To Homepage</span></a>
+					</div>
+				</div><!-- row -->
+			</div><!-- end 404 place -->
 
 @stop
